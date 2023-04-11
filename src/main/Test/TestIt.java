f0eb5352b913385950b29ece8486ea84e44966c6
DavidNiels3n <114468811+DavidNiels3n@users.noreply.github.com>
2023-04-11 10:23:41 +0200

    Calc

---
+++ src/main/Test/TestIt.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestIt {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void testNothing()
+    {
+        assertEquals(12,10,2);
+    }
